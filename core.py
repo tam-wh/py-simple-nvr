7210828c81240df6a4ffee4479d8210d7b37667b
ravetam <rave.tam@gmail.com>
2021-08-15 15:57:06 +0800

    mqtt

--- core.py
+++ core.py
@@ -1,8 +1,12 @@
 import os
+import signal
 import subprocess
 import logging
+import asyncio
 from config import Config
 from mqtt import Mqtt
+from subprocess import PIPE, STDOUT
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -11,72 +15,105 @@ class Core:
     subs = []
 
     def start(self):
+        self.config = Config()
+        self.config.read()
+        self.mqtt = Mqtt(self.config.mqtt_address, self.config.mqtt_port, self.config.mqtt_username, self.config.mqtt_password, self.config.mqtt_topic)
+        self.mqtt.subscribe('pyainvr/actions/stop', self.kill_all)
+        self.mqtt.subscribe('pyainvr/actions/start', self.reload_config)
+
         self.reload_config()
+
+    def kill_all(self):
+        for sub in self.subs:
+            os.kill(sub.pid, signal.SIGINT)
+            sub.wait()
+            logger.warning("Killing process")
+
+        self.subs.clear()
+
+    def log_subprocess_output(self, pipe):
+        for line in iter(pipe.readline, b''): # b'\n'-separated lines
+            logger.info('got line from subprocess: %r', line)
+
+    def run_cam(self, cam):
+        logger.warning(f"Setting up camera {cam.name}")
+
+        cmd = ['ffmpeg', 
+            '-loglevel', self.config.log_level,
+            '-stimeout', '1000000',
+            '-fflags', '+genpts+discardcorrupt',
+            '-use_wallclock_as_timestamps', '1',
+            '-rtsp_transport', 'tcp',
+            '-i', cam.stream]
+
+        if cam.rtsp:
+            rtsp_stream = f'rtsp://{self.config.rtsp_host}:8554/live/{cam.name}'
+            rtsp_cmd = ['-rtsp_transport', 'tcp',
+                '-c:v', 'copy',
+                '-c:a', 'copy',
+                '-f', 'rtsp',
+                rtsp_stream]
+
+            cmd = cmd + rtsp_cmd
+            logger.warning(f"RTSP stream ready here: {rtsp_stream}")
+
+        if cam.rtmp:
+            rtmp_stream = f'rtmp://{self.config.rtmp_host}:1936/live/{cam.name}'
+            rtmp_cmd = ['-c:v', 'copy',
+                '-an',
+                '-f', 'flv',
+                rtmp_stream]
+
+            cmd = cmd + rtmp_cmd
+            logger.warning(f"RTMP stream ready here: {rtmp_stream}")
+
+        if cam.record:
+            rec_cmd = [
+            '-f', 'segment',
+            '-segment_time', f'{self.config.record_segment_length}',
+            '-segment_atclocktime', '1',
+            '-segment_format', 'mp4',
+            '-reset_timestamps', '1',
+            '-strftime', '1',
+            f'{os.path.join(self.config.record_dir, cam.name, )}_%Y%m%d_%H-%M-%S.mp4']   
+
+            cmd = cmd + cam.inputs + rec_cmd
+            logger.warning(f"Setting up recording")    
+
+        logger.info(cmd)
+
+        # proc = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        # self.subs.append(proc)
+        # stdout, stderr = await proc.communicate()
+
+        # logger.info(f'[{cmd!r} exited with {proc.returncode}]')
+        # if stdout:
+        #     logger.info(f'[stdout]\n{stdout.decode()}')
+        # if stderr:
+        #     logger.info(f'[stderr]\n{stderr.decode()}')
+
+        sub = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)            
+        self.subs.append(sub)
+
+        # with sub.stdout:
+        #     self.log_subprocess_output(sub.stdout)
 
     def reload_config(self):
         
-        self.config = Config()
-        self.config.read()
-        
         # Kill existing process
-        for sub in self.subs:
-            sub.kill()
-
-        
-        #self.mqtt = Mqtt(self.config.mqtt_address, self.config.mqtt_port, self.config.mqtt_username, self.config.mqtt_password, self.config.mqtt_topic)
+        self.kill_all()
 
         for cam in self.config.Cameras:
-            logger.warning(f"Setting up camera {cam.name}")
+            self.run_cam(cam)
 
-            cmd = ['ffmpeg', 
-                '-loglevel', self.config.log_level,
-                '-stimeout', '1000000',
-                '-fflags', '+genpts+discardcorrupt',
-                '-use_wallclock_as_timestamps', '1',
-                '-rtsp_transport', 'tcp',
-                '-i', cam.stream]
+        # tasks = []
+        # for cam in self.config.Cameras:
+        #     tasks.append(asyncio.create_task(self.run_cam(cam)))
+        
+        # await asyncio.gather(*tasks)
 
-            if cam.rtsp:
-                rtsp_stream = f'rtsp://{self.config.rtsp_host}:8554/live/{cam.name}'
-                rtsp_cmd = ['-rtsp_transport', 'tcp',
-                    '-c:v', 'copy',
-                    '-c:a', 'copy',
-                    '-f', 'rtsp',
-                    rtsp_stream]
-
-                cmd = cmd + rtsp_cmd
-                logger.warning(f"RTSP stream ready here: {rtsp_stream}")
-
-            if cam.rtmp:
-                rtmp_stream = f'rtmp://{self.config.rtmp_host}:1936/live/{cam.name}'
-                rtmp_cmd = ['-c:v', 'copy',
-                    '-an',
-                    '-f', 'flv',
-                    rtmp_stream]
-
-                cmd = cmd + rtmp_cmd
-                logger.warning(f"RTMP stream ready here: {rtmp_stream}")
-
-            if cam.record:
-                rec_cmd = [
-                '-f', 'segment',
-                '-segment_time', f'{self.config.record_segment_length}',
-                '-segment_atclocktime', '1',
-                '-segment_format', 'mp4',
-                '-reset_timestamps', '1',
-                '-strftime', '1',
-                f'{os.path.join(self.config.record_dir, cam.name, )}_%Y%m%d_%H-%M-%S.mp4']   
-
-                cmd = cmd + cam.inputs + rec_cmd
-                logger.warning(f"Setting up recording")    
-
-            logger.info(cmd)
-            sub = subprocess.Popen(cmd)
-            
-            self.subs.append(sub)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        for sub in self.subs:
-            sub.kill()
-            
+        self.kill_all()
         pass
+
--- mqtt/mqtt.py
+++ mqtt/mqtt.py
@@ -1,14 +1,23 @@
 import paho.mqtt.client as mqtt
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Mqtt:
 
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(self, client, userdata, flags, rc):
         client.subscribe(self.topic,1)
         print("Connected with result code "+str(rc))
 
-    def on_publish(client,userdata,result):
+    def on_publish(self, client,userdata,result):
         print("Data published")
         pass
+
+    def on_message(self, client, userdata, message):
+        logger.warning("Received message '" + str(message.payload) + "' on topic '" + message.topic + "' with QoS " + str(message.qos))
+        action = self.subscriptions.get(message.topic)
+        if action != None:
+            action()
 
     def __init__(self, address, port, username, password, topic) -> None:
         
@@ -16,10 +25,18 @@ class Mqtt:
         self.client = mqtt.Client("pyainvr") 
         self.client.on_connect = self.on_connect
         self.client.on_publish = self.on_publish
+        self.client.on_message = self.on_message
         self.client.username_pw_set(username=username,password=password)
         print("Connecting...")
         self.client.connect(address, port)
         self.client.loop_start()
 
-    def stop():
+        self.subscriptions = {}
+
+    def stop(self):
         self.client.loop_stop() 
+    
+    def subscribe(self, topic, action):
+        self.subscriptions[topic] = action
+        self.client.subscribe(topic, qos=0)
+
